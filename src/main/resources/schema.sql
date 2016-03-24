421dfd43b0349ee9862f322fd9828ccc4496ee78
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-03-24 16:23:09 +0800

    修改SQL语句

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,17 @@
+/*
+ *当服务启动成功后会自动创建实体类里面的表，并插入一条数据
+ * 
+ * */
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `last_accessed` datetime DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 insert into user(username, first_name, last_name, created_date) values ('danveloper', 'Dan', 'Woods', now())
