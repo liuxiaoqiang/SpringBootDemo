860b493a3089cad875b739abc9d1bf96f9035ddf
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-03-24 16:39:07 +0800

    添加mongodb配置说明

--- src/main/java/com/reus/repository/UserRepository.java
+++ src/main/java/com/reus/repository/UserRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.reus.model.User;
 
+/* 当使用非关系型数据库mongodb时要使用下面这个接口
+ * 同时pom文件中还应该有mongodb的驱动包说spring-boot-starter-data-mongodb
+ * public interface UserRepository extends MongoRepository<User, Long> {}
+ */
 public interface UserRepository extends CrudRepository<User, Long> {
 }
